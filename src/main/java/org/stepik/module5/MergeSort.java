55f52b3945871740dea33bb70c190b97cb80285d
zhan_lenovo <zhanybekadiev@gmail.com>
2021-05-29 18:10:14 +0600

    last commit

--- src/main/java/org/stepik/module5/MergeSort.java
+++ src/main/java/org/stepik/module5/MergeSort.java
@@ -2,16 +2,24 @@ package org.stepik.module5;
 
 public class MergeSort {
     public static int[] sort(int[] array) {
-        if (array == null) return null;
-        if (array.length == 0) return new int[0];
-        return sort(array,0,array.length-1);
+        if (array == null) {
+            return null;
+        } else if (array.length == 0) {
+            return new int[0];
+        } else {
+            return sort(array, 0, array.length - 1);
+        }
     }
 
     static int[] sort(int[] array, int low, int high) {
-        if (low == high) return new int[] { array[low] };
-        else if (low + 1 == high) {
-            if (array[low] < array[high]) return new int[] { array[low], array[high] };
-            else return new int[] { array[high], array[low] };
+        if (low == high) {
+            return new int[] { array[low] };
+        } else if (low + 1 == high) {
+            if (array[low] < array[high]) {
+                return new int[] { array[low], array[high] };
+            } else {
+                return new int[] { array[high], array[low] };
+            }
         } else {
             int mid = low + (high - low) / 2;
             return merge(sort(array, low, mid), sort(array, mid + 1, high));
@@ -26,17 +34,20 @@ public class MergeSort {
 
         while (cursor1 < array1.length && cursor2 < array2.length) {
 
-            if (array1[cursor1] <= array2[cursor2])
+            if (array1[cursor1] <= array2[cursor2]) {
                 merged[mergedCursor++] = array1[cursor1++];
-            else
+            } else {
                 merged[mergedCursor++] = array2[cursor2++];
+            }
         }
 
-        while (cursor1 < array1.length)
+        while (cursor1 < array1.length) {
             merged[mergedCursor++] = array1[cursor1++];
+        }
 
-        while (cursor2 < array2.length)
+        while (cursor2 < array2.length) {
             merged[mergedCursor++] = array2[cursor2++];
+        }
 
         return merged;
     }
